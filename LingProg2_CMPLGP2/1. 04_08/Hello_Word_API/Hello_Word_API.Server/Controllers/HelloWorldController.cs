466a5487732b1f00a19674f48c7d033322fb4c48
ppfuark <phfuark2007@gmail.com>
2026-08-04 22:07:54 -0300

    feat: CMPLGP2 (04.08)
    Hello World in Console and in a Web API

--- LingProg2_CMPLGP2/1. 04_08/Hello_Word_API/Hello_Word_API.Server/Controllers/HelloWorldController.cs
+++ LingProg2_CMPLGP2/1. 04_08/Hello_Word_API/Hello_Word_API.Server/Controllers/HelloWorldController.cs
@@ -3,12 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 namespace Hello_Word_API.Server.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/ppfuark")]
     public class HelloWorldController : ControllerBase {
         [HttpGet]
         public IActionResult Get()
         {
             return Ok("Hello World Reload");
+        }
+
+        [HttpGet("{text}")]
+        public IActionResult Get(string text)
+        {
+            return Ok($"Hello World {text}");
         }
     }
 
